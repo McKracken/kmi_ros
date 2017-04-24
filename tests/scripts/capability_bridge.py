ff5c116ca57d2185c7961b92d355be91117b1920
emanuele.bastianelli <bastianelli@ing.uniroma2.it>
2017-04-24 15:14:52 +0100

    added requests

--- tests/scripts/capability_bridge.py
+++ tests/scripts/capability_bridge.py
@@ -4,6 +4,7 @@ import rosnode
 import rosmsg
 import json
 from RosNode import RosNode
+import requests
 
 if __name__ == "__main__":
 	nodelist = rosnode.get_node_names()
@@ -12,7 +13,15 @@ if __name__ == "__main__":
 		# initialise RosNode object
 		node = RosNode(nodename)
 		
+		res = requests.get("http://localhost:5050/onto-rob-server/capabilities",data={"jsonString":node.get_capability_msg()})
+
+		if res.status_code == 200:
+			print "Message successfully sent"
+		else:
+			print "Error while sending: %s %s" % (res.status_code, res.text)
+
 		print json.dumps(node.get_capability_msg())
+		
 		"""msgs, srvs = rosnode info
 	
 		for each msg in msgs:
