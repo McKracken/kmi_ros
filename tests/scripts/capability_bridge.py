0806ed640834b3b286c674302bdf7b9c405bdfbd
emanuele.bastianelli <bastianelli@ing.uniroma2.it>
2017-06-13 11:29:51 +0100

    updating interact_with_master

--- tests/scripts/capability_bridge.py
+++ tests/scripts/capability_bridge.py
@@ -12,15 +12,16 @@ if __name__ == "__main__":
 	for nodename in nodelist:
 		# initialise RosNode object
 		node = RosNode(nodename)
-		
-		res = requests.get("http://localhost:5050/onto-rob-server/capabilities",data={"jsonString":node.get_capability_msg()})
-
-		if res.status_code == 200:
-			print "Message successfully sent"
-		else:
-			print "Error while sending: %s %s" % (res.status_code, res.text)
 
 		print json.dumps(node.get_capability_msg())
+		
+		#res = requests.get("http://localhost:5050/onto-rob-server/capabilities",data={"jsonString":node.get_capability_msg()})
+
+		#if res.status_code == 200:
+		#	print "Message successfully sent"
+		#else:
+		#	print "Error while sending: %s %s" % (res.status_code, res.text)
+
 		
 		"""msgs, srvs = rosnode info
 	
